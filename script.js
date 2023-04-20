1f33f8f52541b675d59d04426b49eb9e2682f4c4
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-20 12:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+
+let timer = document.querySelector('#timer')
+setInterval(() => {
+	let now = new Date()
+	
+	let date = now.getDate()
+	let month = now.getMonth()
+	let year = now.getFullYear()
+
+	let hour = now.getHours()
+	let minutes = now.getMinutes()
+	let seconds = now.getSeconds()
+
+	let time = now.toLocaleTimeString()
+	timer.innerText = `${month}/${date}/${year}, ${time}`
+}, 1000)
